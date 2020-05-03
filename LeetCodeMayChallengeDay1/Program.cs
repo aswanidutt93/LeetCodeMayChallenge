ed942f6c85fcaaf35abb33ad8171ed41abbbf437
aswanidutt93 <aswanidutt93@outlook.com>
2020-05-03 13:43:01 +0530

    Add project files.

---
+++ LeetCodeMayChallengeDay1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMayChallengeDay1
+{
+    /* The isBadVersion API is defined in the parent class VersionControl.
+       bool IsBadVersion(int version); */
+
+    public class Solution : VersionControl
+    {
+        public int FirstBadVersion(int n)
+        {
+            int left = 1;
+            int right = n;
+            while (left < right)
+            {
+                int middle = left + (right - left) / 2;
+                if (!IsBadVersion(middle))
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle;
+                }
+            }
+
+            return left;
+        }
+
+    }
+
+    public class VersionControl
+    {
+        public bool IsBadVersion(int val)
+        {
+            return true;
+        }
+    }
+}
---
+++ LeetCodeMayChallengeDay2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMayChallengeDay2
+{
+    public class Solution
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            int count = 0;
+
+            foreach (var ja in J)
+            {
+                foreach (var sa in S)
+                {
+                    if (ja == sa)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
