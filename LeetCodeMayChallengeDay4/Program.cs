09273611097fe8d3fa677f1df59ae17483721c03
aswanidutt93 <aswanidutt93@outlook.com>
2020-05-04 20:10:43 +0530

    day challenge

---
+++ LeetCodeMayChallengeDay4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMayChallengeDay4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindComplement(5);
+        }
+
+        public static int FindComplement(int num)
+        {
+            return Convert.ToInt32(string.Join("",Convert.ToString(num, 2).Select(x => x == '1' ? '0' : '1')),2);
+        }
+    }
+}
