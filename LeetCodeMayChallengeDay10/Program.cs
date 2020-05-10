3678122a041a61129d71fce8083dc612652f564f
aswanidutt93 <aswanidutt93@outlook.com>
2020-05-10 15:44:44 +0530

    day 10

---
+++ LeetCodeMayChallengeDay10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMayChallengeDay10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var x = new int[][] {[1, 2]; };
+            int[][] array2D = new int[][] { new int[] { 1, 3 }, new int[] { 2, 3 } };
+            var res = FindJudge(3, array2D);
+        }
+
+        public static int FindJudge(int N, int[][] trust)
+        {
+            int[] count = new int[N + 1];
+            foreach (var item in trust)
+            {
+                count[item[0]]--;
+                count[item[1]]++;
+            }
+
+            for (int i = 0; i < N + 1; i++)
+            {
+                if (count[i] == N - 1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ LeetCodeMayChallengeDay7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMayChallengeDay7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static bool IsCousins(TreeNode root, int x, int y)
+        {
+            var ltree = root.left;
+            var rtree = root.right;
+            while (ltree != null && rtree != null)
+            {
+                if ((ltree.val == x || ltree.val == y)
+                   && rtree.val == x || rtree.val == y)
+                {
+                    return true;
+                }
+
+                ltree = ltree.left;
+                rtree = ltree.right;
+            }
+            return false;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
