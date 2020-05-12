497cebb6f1c7468c51bbd66ceb508d1afdd06069
aswanidutt93 <aswanidutt93@outlook.com>
2020-05-12 21:12:04 +0530

    day 12

---
+++ LeetCodeMayChallengeDay12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMayChallengeDay12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
+           var res= SingleNonDuplicate(arr);
+        }
+
+        public static int SingleNonDuplicate(int[] nums)
+        {
+            Dictionary<int, int> d = new Dictionary<int, int>();
+
+            foreach (var num in nums)
+            {
+                if (d.ContainsKey(num))
+                {
+                    d[num]++;
+                }
+                else
+                {
+                    d.Add(num, 1);
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (d[nums[i]]==1){
+                    return nums[i];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
