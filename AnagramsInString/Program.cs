43c52a42a144f0e686dd5bdae3872d133a8f71b6
aswanidutt93 <aswanidutt93@outlook.com>
2020-05-17 15:38:57 +0530

    day17

---
+++ AnagramsInString/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnagramsInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindAnagrams("cbaebabacd", "abc");
+            Console.WriteLine("Hello World!");
+        }
+
+        public static IList<int> FindAnagrams(string s, string p)
+        {
+            List<int> li = new List<int>();
+
+            
+            int[] pArray = new int[26];
+            for (int i = 0; i < p.Length; i++)
+            {
+                pArray[p[i] - 97]++;
+            }
+
+            for (int i = 0; i < s.Length - p.Length; i++)
+            {
+                int[] sArray = new int[26];
+                var sstr = s.Substring(i, p.Length);
+                for (int j = 0; j < sstr.Length; j++)
+                {
+                    sArray[sstr[j] - 97]++;
+                }
+
+                int count = 0;
+                for (int k = 0; k < 26; k++)
+                {
+                    if (pArray[k] == sArray[k])
+                    {
+                        count++;
+                    }
+                }
+
+                if (count == 26)
+                {
+                    li.Add(i);
+                }
+
+
+
+            }
+
+
+            
+            return li;
+        }
+    }
+}
---
+++ OddEvenLinkedList/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace OddEvenLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, null)))));
+            OddEvenList(list);
+        }
+
+        public static ListNode OddEvenList(ListNode head)
+        {
+            ListNode odd = head;
+            ListNode even = new ListNode(head.next.val,null);
+            int oddcount = 1;
+            while (odd.next != null)
+            {
+                if (oddcount % 2 == 0)
+                {
+                    even.next = odd.next.next;
+                   // odd.next = odd.next.next;
+                }
+                else
+                {
+                    odd.next = odd.next.next;
+                }
+                oddcount++;
+            }
+
+
+            return odd;
+
+        }
+
+
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
