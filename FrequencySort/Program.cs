8cfba8ff9f7def1c06dac94b950598650b0b8d0b
aswanidutt93 <aswanidutt93@outlook.com>
2020-05-22 19:45:20 +0530

    freq sort

---
+++ FrequencySort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace FrequencySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FrequencySort("Aaaccc");
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public static string FrequencySort(string s)
+        {
+            return string.Join("", s.GroupBy(x => x).OrderByDescending(gr => gr.Count()).SelectMany(gr => gr));
+        }
+    }
+}
